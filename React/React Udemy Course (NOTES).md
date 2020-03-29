6042cf7418e27c8a05e35a8fb72e64409fa291dd
Paras Agrawal <paras@piktorlabs.com>
2020-03-29 23:37:53 +0545

    JSX Done

--- React/React Udemy Course (NOTES).md
+++ React/React Udemy Course (NOTES).md
@@ -7,3 +7,7 @@ Q) What is Babel?
 A) Babel is a free and open-source JavaScript compiler that is mainly used to convert ECMAScript 2015+ code into a backwards compatible version of JavaScript that can be run by older JavaScript engines. Babel is popular tool for using the newest features of the JavaScript programming language.   
 Type: compiler   
 
+Babel converts the JSX into JS codes(React.createElement) which then converted into HTML elements.   
+
+Q) Why we use className keyword instead of class in JSX?, and does class keyword works?
+A) Yes, class works. But we shouldn't use 'class'. The reason is that it will be confused with 'class' keyword used for creating components. Example, class App extends React.Component
--- React/react/reactlearning/src/index.js
+++ React/react/reactlearning/src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+
+    // Variables' type that we can use or we cannot.
+    const content = 'Hey There';
+    // const content = 123;
+    // const content = ['hi', 'hey'];
+    // const content = [1, 2];
+    // const content = {text: 'hey'}; // ERROR: Objects are not valid as a React child
+
+    return (
+        <div style={{border: '1px solid red'}} className="divStyle">
+            {content}
+
+            {/* <label for="myname">Name</label> */}
+            {/*
+                Warning: Invalid DOM property `for`
+                Similar to className vs class, use 'htmlFor' over for.
+            */}
+            <label htmlFor="myname">Name</label>
+            <input id="myname" />
+            
+        </div>
+    ); 
+    // First Curly braces indicates JS variable and Second Curly braces indicates object
+
+    // class and className both are working.
+    // If we use both together as className="divStyle" class="divStyle1", 
+    // class's styling is overwriting className's styling here as it comes later.
+    // But we shouldn't use 'class'. The reason is that it will be confused with 'class' keyword used for creating components.
+    // Example, class App extends React.Component
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    // document.querySelector('#root')
+    document.getElementById('root')
+)
