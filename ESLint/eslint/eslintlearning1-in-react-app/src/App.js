b7437bf6461a795f42bd2235620eec1756273513
Paras Agrawal <paras@piktorlabs.com>
2020-04-28 17:53:56 +0530

    add some commands to run lint

--- ESLint/eslint/eslintlearning1-in-react-app/src/App.js
+++ ESLint/eslint/eslintlearning1-in-react-app/src/App.js
@@ -4,13 +4,14 @@ import React from 'react';
 // npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier
 // npx install-peerdeps --dev eslint-config-airbnb
 // Then, copied .eslintrc.json and .prettierrc files from 'reactlearning-in-vanilla-js'
+// Created .eslintignore file
+
+// Run 'npx eslint ./src/App.js' to list ESLint errors and warnings.
+// Run 'npx eslint ./src/App.js --fix' to remove errors and warnings as much as ESLint can.
+// Run 'yarn lint' script to remove errors and warnings from all files.
 
 const App = () => {
-  return (
-    <div>
-      ESLint
-    </div>
-  );
-}
+  return <div>ESLint</div>;
+};
 
 export default App;
