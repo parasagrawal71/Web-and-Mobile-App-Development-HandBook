28b01e68744f057df5ea151adea93b50b24c9f8f
Paras Agrawal <paras@piktorlabs.com>
2020-04-28 17:13:03 +0530

    Installed ESLint in existing react project

--- ESLint/eslint/eslintlearning1-in-react-app/src/App.js
+++ ESLint/eslint/eslintlearning1-in-react-app/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Installed dev dependencies:-
+// npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier
+// npx install-peerdeps --dev eslint-config-airbnb
+// Then, copied .eslintrc.json and .prettierrc files from 'reactlearning-in-vanilla-js'
 
 const App = () => {
   return (
