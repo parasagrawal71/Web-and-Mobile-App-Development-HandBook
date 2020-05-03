d2f1af66812681f942c69c75a76477ce87595503
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-03 21:07:13 +0530

    All action creators

--- React and Redux/react/reactlearning1-streams/client/src/actions/index.js
+++ React and Redux/react/reactlearning1-streams/client/src/actions/index.js
@@ -1,21 +1,53 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
-import streams from '../apis/streams';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "./types";
+import streams from "../apis/streams";
 
 export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
-}
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
+  return {
+    type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValues) => async (dispatch) => {
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get("/streams");
+  
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
 }
 
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const fetchStream = (streamId) => async dispatch => {
+    const response = await streams.get(`/streams/${streamId}`);
+  
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+}
 
-    dispatch({ type: CREATE_STREAM, payload: response.data });
-};
+export const editStream = (streamId, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${streamId}`, formValues);
+  
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+}
+
+export const deleteStream = (streamId) => async dispatch => {
+    await streams.delete(`/streams/${streamId}`);
+  
+    dispatch({ type: DELETE_STREAM, payload: streamId });
+}
