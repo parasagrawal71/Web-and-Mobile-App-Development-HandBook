74ee229dc37bcfce84f46853d2050b069d65d028
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-05 00:48:29 +0530

    streamShow working

--- React and Redux/react/reactlearning1-streams/client/src/routes.js
+++ React and Redux/react/reactlearning1-streams/client/src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { BrowserRouter, Route } from "react-router-dom";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "./history";
 
 import StreamList from "./components/streams/StreamList";
@@ -17,12 +17,16 @@ const Routes = () => {
     <Router history={history}>
       <Header />
       <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit/:id" exact component={StreamEdit} />
-        {/* <Route path="/streams/edit/:anything" exact component={StreamEdit} /> */}
-        <Route path="/streams/show" exact component={StreamShow} />
-        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+        <Switch>
+          {/* Switch makes to show only one matching route and leave other matching routes */}
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          {/* <Route path="/streams/edit/:anything" exact component={StreamEdit} /> */}
+          <Route path="/streams/:id" exact component={StreamShow} />
+          {/* "/streams/:id" matches to /streams/{any_value} coz id is variable that can take any value so it is getting matched with "/streams/new" */}
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+        </Switch>
       </div>
       {/* </BrowserRouter> */}
     </Router>
