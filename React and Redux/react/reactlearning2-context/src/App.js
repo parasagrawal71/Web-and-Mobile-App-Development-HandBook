a3da965fc1b15cd714e1ed4a66e709a719a0958d
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-06 22:00:40 +0530

    All required components added

--- React and Redux/react/reactlearning2-context/src/App.js
+++ React and Redux/react/reactlearning2-context/src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import UserCreate from "./components/UserCreate";
 
-function App() {
-  return <div className="App">App</div>;
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange("hindi")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ React and Redux/react/reactlearning2-context/src/components/UserCreate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Field from "./Field";
+import Button from "./Button";
+
+class UserCreate extends React.Component {
+  render() {
+    return (
+      <div className="ui form">
+        <Field />
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default UserCreate;
