495e1d672828568f7e9c47ebb7b2cf4fcfa17fcc
Paras Agrawal <parasagrawal71@gmail.com>
2020-05-17 17:07:02 +0530

    Color got from props goes to inline styling

--- CSS (and SASS or SCSS)/scss/scsslearning0-in-react-app/src/App.js
+++ CSS (and SASS or SCSS)/scss/scsslearning0-in-react-app/src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./App.scss";
 import "./App.css";
 
+const colorVar = "red";
+
 function App() {
+  const assumeItProp = "blue";
+
   return (
     <div className="App">
-      <header className="App-header">Header</header>
-      <div className="App-content">CONTENT</div>
+      <header className="App-header" style={styles.headerColor}>
+        Header
+      </header>
+      <div className="App-content" style={{ color: assumeItProp }}>
+        CONTENT
+      </div>
     </div>
   );
 }
+
+const styles = {
+  headerColor: {
+    color: colorVar,
+  },
+};
 
 export default App;
